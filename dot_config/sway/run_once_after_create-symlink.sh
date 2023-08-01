560a4223e276961ef92294f71a591a3fc4048b01
kev1nbam27 <kevin1bam@web.de>
2023-08-01 14:13:16 +0200

    packages list modified, made scripts idempotent

--- dot_config/sway/run_once_after_create-symlink.sh
+++ dot_config/sway/run_once_after_create-symlink.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# check if dir or symlink already exists
-if [[ -e /usr/share/sway/scripts ]]; then
+# check if symlink already exists
+if [[ -L /usr/share/sway/scripts ]]; then
     exit;
 fi
 
 # check whether we should use sudo or doas
 [ command -v doas &> /dev/null ] && sudo=doas || sudo=sudo
 
+# remove preinstalled sway scripts
+$sudo rm -rf /usr/share/sway/scripts
 echo "Creating symlink '/usr/share/sway/scripts' -> '$(pwd)/scripts'"
 $sudo ln -s $(pwd)/scripts /usr/share/sway/scripts
--- run_once_before_install.sh
+++ run_once_before_install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# check if script has been run before
+if [[ -f $HOME/.config/chezmoi/already_installed ]]; then
+    exit;
+fi
+
 dir=$(pwd)
 # check whether we should use sudo or doas
 [ command -v doas &> /dev/null ] && sudo=doas || sudo=sudo
+echo Using $sudo
 
+echo Checking for updates...
 $sudo pacman -Syu
+echo Installing pacman packages...
 $sudo pacman -S - < .packages_pacman
 
-# install yay
+echo Installing yay
 cd $HOME
 $sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
@@ -15,4 +23,7 @@ cd yay
 $sudo makepkg -si
 
 yay -Syu
+echo Installing yay packages
 yay -S - < .packages_yay
+
+mkdir -p $HOME/.config/chezmoi && touch $_/already_installed
