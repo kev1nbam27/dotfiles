431aa27e0acb8604e9c89198cfee09f30425d165
kev1nbam27 <kevin1bam@web.de>
2023-10-26 21:39:59 +0200

    added catppuccin to nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -113,10 +113,11 @@ require('lazy').setup({
   },
 
   { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'catppuccin/nvim',
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
@@ -126,7 +127,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
